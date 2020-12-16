b8ff80d3c614e21b4ca1ecb860248dedb7330edf
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-12-16 17:00:03 -0500

    initial commit; TODO: fix button pos. - temp setting for screen recording

---
+++ project10/script.js
@@ -0,0 +1,39 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING async/await
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: "application/json",
+        },
+    }
+
+    // const res = await fetch('https://icanhazdadjoke.com/', config)
+    const res = await fetch('https://dog.ceo/api/breeds/image/random', config)
+    
+    const data = await res.json()
+        
+    // jokeEl.innerHTML = data.joke
+    jokeEl.innerHTML = '<img src="'+data.message+'">'
+}
+
+
+// USING .then()
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: "application/json",
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
